27fbc3661771150d0d473f20441277067e272746
yasmeinelkenany <yasmeinelkenany@gmail.com>
2024-09-01 07:28:08 +0300

    intial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header'
+import './styles/app.scss'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header ></Header>
+        </div>
+    </>
+  )
+}
+
+export default App
